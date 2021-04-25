0e96ced6c1467636bf0a3dc0baed627e85d41bb4
Ricardo Flores <rf.cabal@gmail.com>
2021-04-25 20:44:39 -0300

    Modify remote of submodule

--- generators/Fullstack-Node-React-Archetype/config/db.js
+++
@@ -1,6 +0,0 @@
-/*
-module.exports = {
-  secret: ' ',
-  database: process.env.NODE_ENV === 'develop' ? 'mongodb://localhost/{db-test}' : 'mongodb://localhost/{db}'
-};
-*/
--- generators/Fullstack-Node-React-Archetype/routes/api/welcome/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get("/", (req, res) => {
-  res.send("Welcome this is test for this api");
-});
-
-module.exports = router;
